48e8396a7d487a9f31151004c48747385132ef26
Devender <98731315+devendersing@users.noreply.github.com>
2023-11-03 16:57:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+'''
+addition "+"
+substraction "-"
+mutiply "*"
+Divide "/"
+'''
+num1=int(input(" Enter the number"))
+num2=int(input("Enter the number"))
+o=input("enter the opr" (+,-,*,/
+if opr =="+":
+      print(num1+num2)
+   elif opr =="-"
+      print(num1-num2)
+   elif opr =="*"
+      print (num1*num2)
+else:
+print("invalid opr")
